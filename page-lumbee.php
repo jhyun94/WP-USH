cbe02f4f3e295b54caee049c8495f3e705f89d8f
jhyun94 <hyunjonathan@gmail.com>
2017-09-23 15:17:16 -0500

    fixed lumbee, cannot click and not dynamic yet

--- page-lumbee.php
+++ page-lumbee.php
@@ -27,84 +27,84 @@ get_header();
       <div class="row">
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/7610291856_931fb9f461_k.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/7610291856_931fb9f461_k.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/children-82272_1280.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/children-82272_1280.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/human-rights.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/human-rights.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/old-books-436498_1280.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/old-books-436498_1280.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/hunger-682834_1280.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/hunger-682834_1280.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/africa-education.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/africa-education.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/afghani-60798_1280.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/afghani-60798_1280.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/boy-60729_1280.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/boy-60729_1280.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/child-207573_1280.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/child-207573_1280.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/children-60654_1280.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/children-60654_1280.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/doll-87407_1280.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/doll-87407_1280.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
 
         <div class="col-md-3">
           <div class="img__wrap">
-            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/girl-62328_1280.jpg" class="img-responsive">
+            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/09/girl-62328_1280.jpg" class="img-responsive video-btn">
             <p class="img__description">TEST</p>
           </div>
         </div>
