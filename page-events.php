c3fe0de3a71d026051d5bf7b446566e0bc293617
jhyun94 <hyunjonathan@gmail.com>
2017-09-29 11:46:05 -0500

    event page dyanmic

--- page-events.php
+++ page-events.php
@@ -4,7 +4,8 @@
 
 */
 
-
+$main_image = get_field('main_image');
+$main_content = get_field('main_content');
 $event_popup = get_field('event_popup');
 get_header(); ?>
 
@@ -25,20 +26,13 @@ get_header(); ?>
 
         <div class="col-md-6">
 
-          <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/eventspage.png" alt="" class="img-responsive">
+          <img src= <?php echo $main_image['url'] ?> alt="" class="img-responsive">
 
         </div>
 
         <div class="col-md-6">
           <h2 class="title-style-2">Events! <span class="title-under"></span></h2>
-          <p><strong> There are many ways to see us! </strong></p>
-          <p><strong>Past Shows:</strong> we have been fortunate to show our films in from the small coffee shops, to universities, and to famous locations. &nbsp;Come see where we have been! </p>
-          <p>&nbsp;<strong>Upcoming Screenings:</strong> Coming to a theater near you! Take a look at th next locations where USH will be presenting. </p>
-          <p><strong>Interactives:</strong> We are always on the road, showing our films and talking about how we work. &nbsp;Join us for one of these talks where we answer whatever questions that you have. </p>
-          <p data_temp_dwid="1"><strong data_temp_dwid="2">Organize:</strong> Maybe it is Hispanic Heritage Month, or Native American Month.  Perhaps it is a time to inspire those around you to learn more about Indigenous cultures and tradtiions.  You can invite us to speak with you, set up workshops, or help you start your own chapter.</p>
-          <p>What is in it for you? &nbsp;Collaborating with us, you gain valuable nonprofit development experience by building social media strategies, planning business models, developing online platforms and initiating marketing and fundraising campaigns. &nbsp;You also get to be part of a startup organization that is going to change the way indigenous youth learn and lead themselves and their communities! &nbsp; </p>
-            We look forward to working with you!!
-          </p>
+          <p> <?php echo $main_content ?> </p>
         </div>
 
       </div> <!-- /.row -->
