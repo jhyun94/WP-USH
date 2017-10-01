466a0468a526e5ca040f58f15d1188f846790358
jhyun94 <hyunjonathan@gmail.com>
2017-10-01 14:48:52 -0500

    newsletter on report page is dynamic

--- page-reports.php
+++ page-reports.php
@@ -71,24 +71,16 @@ get_header(); ?>
           
           <?php $annual_array = new WP_Query( array( 'post_type' => 'annual_reports') ); ?>
           <?php while( $annual_array->have_posts() ) : $annual_array->the_post(); ?>
-            <p> <a href="<?php the_field('link') ?>" target="_blank"> <?php the_title() ?></a> </p>
+            <p><span> <a href="<?php the_field('link') ?>" target="_blank"> <?php the_title() ?></a></span></p>
           <?php endwhile; ?>
 
 
           <h2>Newsletters:</h2>
-          <p><span><a href="http://mailchi.mp/12b5af2c975e/2016-annual-report-2756018" style="color: #328d88;" target="_blank">May 2017</a></span></p>
-          <p><span><a href="http://us3.campaign-archive2.com/?u=73e8b8cb16ba80affc09c7cb6&id=7948eb6249" style="color: #328d88;" target="_blank">October 2016</a></span></p>
-          <p><span><a href="http://us3.campaign-archive2.com/?u=73e8b8cb16ba80affc09c7cb6&id=7a30c93771" style="color: #328d88;" target="_blank">August 2016</a></span></p>
-          <p><span><a href="http://us3.campaign-archive1.com/?u=73e8b8cb16ba80affc09c7cb6&id=8bf6c32647" style="color: #328d88;" target="_blank">May 2016</a></span></p>
-          <p><span><a href="http://us3.campaign-archive2.com/?u=73e8b8cb16ba80affc09c7cb6&id=a0e39813f5" style="color: #328d88;" target="_blank">March 2016</a></span></p>
-          <p><span><a href="http://us3.campaign-archive2.com/?u=73e8b8cb16ba80affc09c7cb6&id=248aaa56e1" style="color: #328d88;" target="_blank">December 2015</a></span></p>
-          <p><span><a href="http://us3.campaign-archive1.com/?u=73e8b8cb16ba80affc09c7cb6&id=6f5803245f" style="color: #328d88;" target="_blank">November 2015</a></span></p>
-          <p><span><a href="http://us3.campaign-archive1.com/?u=73e8b8cb16ba80affc09c7cb6&id=ab10ee01ce" style="color: #328d88;" target="_blank">October 2015</a></span></p>
-          <p><span><a href="http://us3.campaign-archive2.com/?u=73e8b8cb16ba80affc09c7cb6&id=7c6d7fda4e" style="color: #328d88;" target="_blank">September 2015</a></span></p>
-          <p><span><a href="http://us3.campaign-archive2.com/?u=73e8b8cb16ba80affc09c7cb6&id=e52e8df199" style="color: #328d88;" target="_blank">August 2015</a></span></p>
-          <p><span><a href="http://us3.campaign-archive2.com/?u=73e8b8cb16ba80affc09c7cb6&id=d3883cf186" style="color: #328d88;" target="_blank">July 2015</a></span></p>
-          <p><span><a href="http://us3.campaign-archive1.com/?u=73e8b8cb16ba80affc09c7cb6&id=130e03905a" style="color: #328d88;" target="_blank">June 2015</a></span></p>
-          <p><span><a href="http://us3.campaign-archive1.com/?u=73e8b8cb16ba80affc09c7cb6&id=99a690b37a" style="color: #328d88;" target="_blank">May 2015</a></span></p>
+          <?php $newsletter_array = new WP_Query( array( 'post_type' => 'newsletter') ); ?>
+          <?php while( $newsletter_array->have_posts() ) : $newsletter_array->the_post(); ?>
+            <p><span> <a href="<?php the_field('link') ?>" target="_blank"> <?php the_title() ?></a></span></p>
+          <?php endwhile; ?>
+
 
 
           </div>
