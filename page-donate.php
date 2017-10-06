12c604ddb5f5b6ed1215a00cae30a77e2618734b
jhyun94 <hyunjonathan@gmail.com>
2017-10-06 18:14:34 -0500

    read more part on donate page is dynamic

--- page-donate.php
+++ page-donate.php
@@ -52,36 +52,16 @@ get_header(); ?>
         <div class="container">
           <h2 class="title-style-1">Funding levels<span class="title-under"></span></h2>
           <div class="row">
-
-                <div class="col-md-3 col-sm-6">
-
-                    <div class="cause">
-
-                        <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/uncovering.png" alt="" class="cause-img">
-
-                        <h4 class="cause-title"><a href="#">Key to Critical Inquiry</a></h4>
-                        <div class="cause-details"> Develops the critical eye, leading youth to inquire about how media represents them and define how they envision expressing their worlds. </div>
-
-                        <div class="btn-holder text-center">
-
-                          <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#donateModal"> READ MORE</a>
-                          
-                        </div>
-
-                        
-
-                    </div> <!-- /.cause -->
-                    
-                </div> 
-
-                <div class="col-md-3 col-sm-6">
+            <?php $donate_array = new WP_Query( array( 'cat' => 14) ); ?>
+            <?php while ($donate_array->have_posts() ) : $donate_array->the_post() ?>
+              <div class="col-md-3 col-sm-6">
 
                     <div class="cause">
 
-                        <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/reviving.png" alt="" class="cause-img">
+                        <img src="<?php the_field('image') ?>" alt="" class="cause-img">
 
-                        <h4 class="cause-title"><a href="#">Key to Creative Expression</a></h4>
-                        <div class="cause-details"> Sparks a creative vision, facilitating youth as agents to capture, represent, &amp; revitalize their cultural knowledges, traditions, and languages,</div>
+                        <h4 class="cause-title"><?php the_field('title') ?></h4>
+                        <div class="cause-details"> <?php the_field('content') ?></div>
 
                         <div class="btn-holder text-center">
 
@@ -95,48 +75,7 @@ get_header(); ?>
                     
                 </div>
 
-
-                <div class="col-md-3 col-sm-6">
-
-                    <div class="cause">
-
-                        <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/sharing.png" alt="" class="cause-img">
-
-                        <h4 class="cause-title"><a href="#">Key to Global Knowledge</a></h4>
-                        <div class="cause-details"> Unlocks gateways to foster cultural understanding, and restore cultural knowledge across geographical &amp; generational bound-aries. </div>
-
-                        <div class="btn-holder text-center">
-
-                          <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#donateModal"> READ MORE</a>
-                          
-                        </div>
-
-                        
-
-                    </div> <!-- /.cause -->
-                    
-                </div>
-
-                <div class="col-md-3 col-sm-6">
-
-                    <div class="cause">
-
-                        <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/leading.png" alt="" class="cause-img">
-
-                        <h4 class="cause-title"><a href="#">Key to Social Change</a></h4>
-                        <div class="cause-details">Unleashes youth-led quests to raise awareness about community issues through film, joining together in a campaign for community change. </div>
-
-                        <div class="btn-holder text-center">
-
-                          <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#donateModal"> READ MORE</a>
-                          
-                        </div>
-
-                        
-
-                    </div> <!-- /.cause -->
-                    
-                </div>
+            <?php endwhile; ?>
 
             </div>
 
