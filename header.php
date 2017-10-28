9074595d5b02da8d60dcfe07ddf2a9c4a9af7a93
jhyun94 <hyunjonathan@gmail.com>
2017-10-28 14:28:23 -0500

    donate button on header works

--- header.php
+++ header.php
@@ -121,7 +121,20 @@
                          <li class="submenu-item"><a href="http://50.87.248.187/~twooneu9/USHWP/news">Publications </a></li>
                          <li class="submenu-item"><a href="http://50.87.248.187/~twooneu9/USHWP/reports">Reports </a></li>
                       </ul>
-                      <li><a href="#" class="btn btn-lg btn-secondary">DONATE</a></li>
+                      <li>
+                        <div class="header_donate">
+                          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                          <input type="hidden" name="cmd" value="_donations">
+                          <input type="hidden" name="business" value="unlockingsilenthistories@gmail.com">
+                          <input type="hidden" name="lc" value="US">
+                          <input type="hidden" name="item_name" value="Unlocking Silent Histories">
+                          <input type="hidden" name="no_note" value="0">
+                          <input type="hidden" name="currency_code" value="USD">
+                          <input type="hidden" name="bn" value="PP-DonationsBF:donate.png:NonHostedGuest">
+                          <button name="submit" class="btn btn-lg btn-secondary">DONATE</button>
+                          </form>
+                        </div>
+                      </li>
             </li> </ul>
                   
                 </div> <!-- /#navbar -->
