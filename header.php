afc46916617138d679a93dbe39687b0f09361e0b
jhyun94 <hyunjonathan@gmail.com>
2017-12-05 17:16:35 -0600

    changed donate to invest on header

--- header.php
+++ header.php
@@ -106,7 +106,7 @@
                     <ul class="submenu">
                          <li class="submenu-item"><a href="http://50.87.248.187/~twooneu9/USHWP/collaborate/">Collaborate</a></li>
                          <li class="submenu-item"><a href="http://50.87.248.187/~twooneu9/USHWP/events">Events </a></li>
-                         <li class="submenu-item"><a href="http://50.87.248.187/~twooneu9/USHWP/donate">Donate </a></li>
+                         <li class="submenu-item"><a href="http://50.87.248.187/~twooneu9/USHWP/donate">Invest </a></li>
                       </ul>
             </li>
                    <li class="has-child"><a href="#">LEADERSHIP</a>
@@ -125,13 +125,13 @@
                         <div class="header_donate">
                           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                           <input type="hidden" name="cmd" value="_donations">
-                          <input type="hidden" name="business" value="info@unlockingsilenthistories.org.">
+                          <input type="hidden" name="business" value="info@unlockingsilenthistories.org">
                           <input type="hidden" name="lc" value="US">
                           <input type="hidden" name="item_name" value="Unlocking Silent Histories">
                           <input type="hidden" name="no_note" value="0">
                           <input type="hidden" name="currency_code" value="USD">
                           <input type="hidden" name="bn" value="PP-DonationsBF:donate.png:NonHostedGuest">
-                          <button name="submit" class="btn btn-lg btn-secondary">DONATE</button>
+                          <button name="submit" class="btn btn-lg btn-secondary">INVEST</button>
                           </form>
                         </div>
                       </li>
