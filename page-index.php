e25fa51ca4fa7b9dc0c62b4973100716de2fa2fd
jhyun94 <hyunjonathan@gmail.com>
2017-12-05 17:13:16 -0600

    changed donate to invest on home page

--- page-index.php
+++ page-index.php
@@ -63,15 +63,15 @@ $sub_heading = get_field('sub_heading');
                   <a href="http://50.87.248.187/~twooneu9/USHWP/missions/" class="btn btn-lg btn-secondary hidden-xs bounceInUp animated slow" >English</a><a href="/missiones" class="btn btn-lg btn-secondary hidden-xs bounceInUp animated slow" data-toggle="modal" data-target="#mission.html">Español</a>
                  <br> 
                   <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                          <input type="hidden" name="cmd" value="_donations">
-                          <input type="hidden" name="business" value="info@unlockingsilenthistories.org">
-                          <input type="hidden" name="lc" value="US">
-                          <input type="hidden" name="item_name" value="Unlocking Silent Histories">
-                          <input type="hidden" name="no_note" value="0">
-                          <input type="hidden" name="currency_code" value="USD">
-                          <input type="hidden" name="bn" value="PP-DonationsBF:donate.png:NonHostedGuest">
-                          <button name="submit" class="btn btn-lg btn-secondary">DONATE</button>
-                          </form>
+                    <input type="hidden" name="cmd" value="_donations">
+                    <input type="hidden" name="business" value="info@unlockingsilenthistories.org">
+                    <input type="hidden" name="lc" value="US">
+                    <input type="hidden" name="item_name" value="Unlocking Silent Histories">
+                    <input type="hidden" name="no_note" value="0">
+                    <input type="hidden" name="currency_code" value="USD">
+                    <input type="hidden" name="bn" value="PP-DonationsBF:donate.png:NonHostedGuest">
+                    <button name="submit" class="btn btn-lg btn-secondary">Invest</button>
+                  </form>
                 </div> <!-- /.carousel-caption -->
 
               </div>
