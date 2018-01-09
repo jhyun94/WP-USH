3dd4e750bbbea98af68222b5cb55db87d8977797
jhyun94 <hyunjonathan@gmail.com>
2018-01-09 16:48:20 -0600

    seperated vision from mission page and put on focus page

--- page-mission.php
+++ page-mission.php
@@ -183,23 +183,6 @@ get_header(); ?>
        
      <!-- /.about-us -->
 
-<div>
-  <div class="container">
-    <h2 class="title-style-1">OUR VISION<span class="title-under"></span></h2>
-    <div class="row">
-      <?php $vision_video = new WP_Query( array('cat' => 22) ); ?>
-        <?php while ( $vision_video->have_posts() ) : $vision_video->the_post() ?>
-          <div class="col-md-6">
-            <p class="vision-content"><?php the_field('video_content') ?></p>
-          </div>
-          <div class="col-md-6">
-            <p style="text-align: center;"><iframe width="500" height="280" frameborder="0" src="<?php the_field(video_source) ?>"><br />
-            </iframe></p>
-          </div>
-        <? endwhile; ?>
-    </div>
-  </div>
-</div>
 
 
 
