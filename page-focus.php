de82e902cfb3b4aeb71e0a4b6ded272b9be0260b
jhyun94 <hyunjonathan@gmail.com>
2018-01-09 17:00:08 -0600

    redesigned focus page

--- page-focus.php
+++ page-focus.php
@@ -9,11 +9,12 @@ get_header(); ?>
   <div class="container">
     <h2 class="title-style-1">OUR VISION<span class="title-under"></span></h2>
     <div class="row">
+    	<div class="col-md-6">
+			  <p style="text-align: center;&nbsp;italics">We envision a future where Indigenous youth leaders around the world inspire teams of young people to capture, revive, and reinvigorate their cultures, traditions, and languages through creating and discussing documentary films. </p>
+			  <p style="text-align: center; italics"> We imagine these Indigenous youth leaders making social impacts, locally and globally by sharing their films across generational and geographical boundaries.</p> <p style="text-align: center; italics"> Listen to their visions!</p>
+			</div>
       <?php $vision_video = new WP_Query( array('cat' => 22) ); ?>
         <?php while ( $vision_video->have_posts() ) : $vision_video->the_post() ?>
-          <div class="col-md-6">
-            <p class="vision-content"><?php the_field('video_content') ?></p>
-          </div>
           <div class="col-md-6">
             <p style="text-align: center;"><iframe width="500" height="280" frameborder="0" src="<?php the_field(video_source) ?>"><br />
             </iframe></p>
