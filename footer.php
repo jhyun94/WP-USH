34794578fc1aab5f762c17616853bffedefea6a7
jhyun94 <hyunjonathan@gmail.com>
2018-02-08 17:54:26 -0600

    updated footer by donna

--- footer.php
+++ footer.php
@@ -16,12 +16,12 @@
 
     <footer class="main-footer">
 
-        <div class="footer-top">
+        <div class="footer-top"> 
             
         </div>
 
 
- <div class="footer-main">
+        <div class="footer-main">
             <div class="container">
                 
                 <div class="row">
@@ -29,8 +29,8 @@
 
                         <div class="footer-col">
                             
-							<p></p>
-							<img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/USHLogo.png" width="300" height="195" alt=""/>
+                            <p></p>
+                            <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2018/02/2018-Logo-Color_English.png" alt="logo"  width="250" height="250" alt=""/>
 
 
                             <div class="footer-content">
@@ -45,30 +45,7 @@
 
                         <div class="footer-col">
 
-                            <h4 class="footer-title">LAST TWEETS <span class="title-under"></span></h4>
-
-                            <div class="footer-content">
-                                <ul class="tweets list-unstyled">
-                                    <li class="tweet"> 
-
-                                        20 Surprise Eggs, Kinder Surprise Cars 2 Thomas Spongebob Disney Pixar  http://t.co/fTSazikPd4 
-
-                                    </li>
-
-                                    <li class="tweet"> 
-
-                                        20 Surprise Eggs, Kinder Surprise Cars 2 Thomas Spongebob Disney Pixar  http://t.co/fTSazikPd4 
-
-                                    </li>
-
-                                    <li class="tweet"> 
-
-                                        20 Surprise Eggs, Kinder Surprise Cars 2 Thomas Spongebob Disney Pixar  http://t.co/fTSazikPd4 
-
-                                    </li>
-
-                                </ul>
-                            </div>
+                            
                             
                         </div>
 
@@ -87,20 +64,37 @@
                                     
                                     <div class="footer-form" >
                                     
-                                    <div class="form-messages">
-                                      </div>
-                                      <form action="<?php bloginfo('template_directory'); ?>/php/mail.php" class="ajax-form" method="post">
+                                    <form action="../../UnlockingSilentHistories/HTML/php/mail.php" class="ajax-form">
+
                                         <div class="form-group">
-                                          <input type="text" class="form-control" name="name" placeholder="Name" required>
+                                            <input type="text" name="name" class="form-control" placeholder="Name" required>
                                         </div>
+
+                                         <div class="form-group">
+                                            <input type="email" name="email" class="form-control" placeholder="E-mail" required>
+                                        </div>
+
                                         <div class="form-group">
-                                          <input type="text" class="form-control" name="email" placeholder="Email" required>
+                                            <textarea name="message" class="form-control" placeholder="Message" required></textarea>
                                         </div>
-                                        <div class="form-group">
-                                          <textarea class="form-control" name="message" placeholder="message" required></textarea>
+
+                                        <div class="form-group alerts">
+                        
+                                            <div class="alert alert-success" role="alert">
+                                              
+                                            </div>
+
+                                            <div class="alert alert-danger" role="alert">
+                                              
+                                            </div>
+                                            
                                         </div>
-                                        <button type="submit" class="btn btn-submit">Submit</button>
-                                      </form>
+
+                                         <div class="form-group">
+                                            <button type="submit" class="btn btn-submit pull-right">Send message</button>
+                                        </div>
+                                        
+                                    </form>
 
                                 </div>
 
@@ -124,15 +118,11 @@
 
         <div class="footer-bottom">
 
-            <div class="container text-right"> UnlockingSilentHistories @ copyrights 2017 </div>
+            <div class="container text-right"> UnlockingSilentHistories @ copyrights 2018 </div>
         </div>
         
     </footer> <!-- main-footer -->
 
-
-
-
-       
         
         <!-- jQuery -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
