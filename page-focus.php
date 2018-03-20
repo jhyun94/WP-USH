dcf3c80eff5fc85f1558377cd1348d88828ad799
jhyun94 <hyunjonathan@gmail.com>
2018-03-20 18:11:42 -0500

    fixed broken image on focus page

--- page-focus.php
+++ page-focus.php
@@ -2,28 +2,108 @@
 /*
 
 
-Template Name: Focus
+Template Name: Focus2
 
 */
 $youth = get_field('youth');
 $work = get_field('work');
 get_header(); ?>
+
 <div class="page-heading text-center">
 
     <div class="container zoomIn animated">
-      <h1 class="page-title">MISSION &amp; VISION<span class="title-under"></span></h1>
-      <p class="page-description"> JOIN US: BE PART OF THE STORY! </p>
+      <h1 class="page-title">FOCUS<span class="title-under"></span></h1>
+      <p class="page-description"> Youth Voice | Indigenous Film </p>
     </div>
 
 </div>
 
       
 
-      <div class="container">  
+<div class="container">  
 <div class="row">
   </div></div>
 
       <div class="main-container">
+      
+      <div class="container">
+          <h2 class="title-style-1">WHAT WE DO<span class="title-under"></span></h2>
+    </div>
+    
+        <div class="section-home home-reasons">
+
+        <div class="container">
+
+            <div class="row">
+                
+                <div class="col-md-6">
+
+                    <div class="reasons-col animate-onscroll fadeIn ">
+
+                        <img src="https://unlockingsilenthistories.org/wp-content/uploads/2018/03/DSC09692.png" alt="">
+
+                        <div class="reasons-titles">
+
+                            <h3 class="reasons-title">Indigenous Media Producers</h3>
+                            <h5 class="reason-subtitle">Telling stories from Within</h5>
+                            
+                        </div>
+
+                        <div class="on-hover hidden-xs">
+                           
+                           <p> Participating youth are using media to discover their cultures, traditions, and knowledges first hand within their communities, a learning model aimed to overcome the indigenous vs non-indigenous education gap.</p>
+
+                                <p></p><p><ul> <li>Non-Indigenous students receive 3.4 years more schooling than Indigenous students</li><li>51.4% school drop-out of indigenous students</li><li>
+              14:8 ratio of Indigenous vs. non-Indigenous people who are working without an education</li><li>53.5% : 32.2% ratio of indigenous vs. non-Indigenous youth aged 15-19 who do not complete primary education</li></ul> 
+                            
+              
+                                
+                        </div>
+                    </div>
+                    
+                </div>
+
+
+
+
+                <div class="col-md-6">
+
+                    <div class="reasons-col animate-onscroll fadeIn">
+
+                        <img src="https://unlockingsilenthistories.org/wp-content/uploads/2018/02/20150813_101948.png" alt="">
+
+                        <div class="reasons-titles">
+
+                            <h3 class="reasons-title">Indigenous Leadership Development</h3>
+                            <h5 class="reason-subtitle">One Country at a Time</h5>
+                        </div>
+
+                        <div class="on-hover hidden-xs">
+                            
+                                 <p>We cultivate communities of Indigenous youth leaders who harness the power of media to bring awareness to important cultural traditions and social issues in order to address the following statistics: </p>
+
+
+              <p></p><ul> <li>24:14 ratio of Indigenous youth who are working vs. attending school</li><li>2:1 ratio of non-indigenous vs. indigenous wages</li><li>
+              25-50 % of the income gap between Indigenous and non-indigenous people is cited as “due to discrimination and non-observable characteristics, such as quality of schooling”</li><li>Indigenous people make up 5% of the world’s population and 15% of the world’s population living in poverty </li></ul> 
+
+                          
+       
+     
+
+                        </div>
+
+
+                    </div>
+                    
+                </div>
+
+
+            </div>
+          
+  
+
+        </div>
+
 
     <div class="container">
 
@@ -97,21 +177,21 @@ get_header(); ?>
           <h2 class="title-style-1">PRESENTATION VENUES<span class="title-under"></span></h2>
           <ul class="owl-carousel list-unstyled list-sponsors">
 
-            <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/ArtinBloom.png" alt=""></li>
-             <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/AFS.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/atitlan.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/aviary.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/BHCC.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/casahererra.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/CECAP.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/cenote.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/espanol.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/flytrap.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/galeria.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/jos.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/piano.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/NMAI.png" alt=""></li>
-              <li> <img src="http://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/UNCW.png" alt=""></li>
+            <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/ArtinBloom.png" alt=""></li>
+             <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/AFS.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/atitlan.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/aviary.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/BHCC.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/casahererra.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/CECAP.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/cenote.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/espanol.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/flytrap.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/galeria.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/jos.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/piano.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/NMAI.png" alt=""></li>
+              <li> <img src="https://50.87.248.187/~twooneu9/USHWP/wp-content/uploads/2017/08/UNCW.png" alt=""></li>
               
 
             </ul>
